9f54cb556881a33274afd6c6a651b5cdeaa7b6f3
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-03-05 01:24:16 +0900

    ui 패키지 생성 후 MainActivity 위치 변경

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/main/MainActivity.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nakyoung.androidclientdevelopment
+package com.nakyoung.androidclientdevelopment.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nakyoung.androidclientdevelopment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

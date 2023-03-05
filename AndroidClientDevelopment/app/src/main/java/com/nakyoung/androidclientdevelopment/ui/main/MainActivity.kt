1f2ed248c599c361ee0fe85a39eaf4ec65d7f6da
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-03-05 12:13:40 +0900

    MainActivity onCreate 수정
    바인딩 및 Activity 이름 연결

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/main/MainActivity.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/main/MainActivity.kt
@@ -2,12 +2,22 @@ package com.nakyoung.androidclientdevelopment.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.viewbinding.ViewBinding
 import com.nakyoung.androidclientdevelopment.R
+import com.nakyoung.androidclientdevelopment.databinding.ActivityMainBinding
+import com.nakyoung.androidclientdevelopment.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        thisActivityString = this.localClassName
 
+        setContentView(binding!!.root)
 
+        Logger("생성완료 ")
     }
+
 }

26c9d31a73517c883390b82e3649e8ce68fcf3a4
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-03-05 12:27:15 +0900

    MainActivity binding 변수 생성 및 할당

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/main/MainActivity.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/main/MainActivity.kt
@@ -4,20 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.navigation.NavigationBarView
 import com.nakyoung.androidclientdevelopment.R
 import com.nakyoung.androidclientdevelopment.databinding.ActivityMainBinding
 import com.nakyoung.androidclientdevelopment.ui.base.BaseActivity
 
 class MainActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         thisActivityString = this.localClassName
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding!!.root)
 
+        //bottomNavigationSetting
+        navigationSetting(binding.navView)
         Logger("생성완료 ")
     }
 
+    override fun navigationSetting(navigationBarView: NavigationBarView) {
+    }
 }

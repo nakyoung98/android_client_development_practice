ba6544988edb3ca426ab9866275868c861cc07eb
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-03-05 12:39:50 +0900

    MENU enum 생성

---
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/statics/MENU.kt
@@ -0,0 +1,9 @@
+package com.nakyoung.androidclientdevelopment.statics
+
+import com.nakyoung.androidclientdevelopment.R
+
+enum class MENU(val id:Int) {
+    TODAY(R.id.today),
+    TIME_LINE(R.id.timeline),
+    PROFILE(R.id.profile)
+}

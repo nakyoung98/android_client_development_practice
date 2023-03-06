ed4ade6fbebfbefc5263aa2bb4a530cc8bf52e84
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-06 11:23:44 +0900

    Update HelloWorld.kt
    date를 string -> date

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/api/response/HelloWorld.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/api/response/HelloWorld.kt
@@ -1,10 +1,11 @@
 package com.nakyoung.androidclientdevelopment.api.response
 
 import android.os.Message
+import java.util.Date
 
 /**
  * GSON을 초기화한 후 fromJson에 JSon문자열과 .class를 넘겨주면 객체를 만들고
  * 알아서 값을 채워줌
  * **/
-data class HelloWorld(val date: String, val message: String) {
+data class HelloWorld(val date: Date, val message: String) {
 }

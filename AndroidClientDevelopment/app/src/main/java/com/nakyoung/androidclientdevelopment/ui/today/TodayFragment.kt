23bc26081fdf0c9864d5b2c714a4dbf784c1c139
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-06 10:16:09 +0900

    today fragment viewbinding처리

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/today/TodayFragment.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/ui/today/TodayFragment.kt
@@ -1,6 +1,26 @@
 package com.nakyoung.androidclientdevelopment.ui.today
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nakyoung.androidclientdevelopment.databinding.FragmentTodayBinding
 import com.nakyoung.androidclientdevelopment.ui.base.BaseFragment
 
 class TodayFragment : BaseFragment(){
+    private var binding: FragmentTodayBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTodayBinding.inflate(inflater,container,false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
 }

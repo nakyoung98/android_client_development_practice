4a126113191d59cb5741c5256afed42f8756dc74
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-08 10:35:11 +0900

    Create Answer.kt
    답변 데이터클래스 생성

---
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/api/response/Answer.kt
@@ -0,0 +1,13 @@
+package com.nakyoung.androidclientdevelopment.api.response
+
+import java.time.LocalDate
+import java.util.*
+
+data class Answer(
+    val qid: LocalDate,
+    val uid: String,
+    val text: String,
+    val photo: String?,
+    val updatedAt: Date,
+    val createAt: Date,
+)

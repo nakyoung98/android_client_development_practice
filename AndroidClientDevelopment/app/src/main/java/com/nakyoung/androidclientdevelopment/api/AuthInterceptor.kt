6e408463f0e79a660f1f10844199458c70826c0b
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-08 17:47:08 +0900

    Tag에 따른 행동 분류

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/api/AuthInterceptor.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/api/AuthInterceptor.kt
@@ -1,6 +1,7 @@
 package com.nakyoung.androidclientdevelopment.api
 
 import com.nakyoung.androidclientdevelopment.manager.AuthManager
+import com.nakyoung.androidclientdevelopment.statics.AuthType
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -12,9 +13,20 @@ class AuthInterceptor: Interceptor {
         val request = chain.request()
         val builder = request.newBuilder()
 
-        AuthManager.accessToken?.let{ token ->
-            builder.header("Authorization", "Bearer $token")
+
+        // 질문을 가져올 때 Authorization이 있는지
+        // 토큰을 갱신할 때 Authrization 헤더가 없는지
+        // 로그인된 상태에서 다시 로그인을 했을 때 Authorization 헤더가 없는지 확인
+        val authType = request.tag(AuthType::class.java) ?: AuthType.ACCESS_TOKEN
+        when (authType) {
+            AuthType.NO_AUTH -> {}
+            AuthType.ACCESS_TOKEN -> {
+                AuthManager.accessToken?.let { token ->
+                    builder.header("Authorization", "Bearer $token")
+                }
+            }
         }
+
         return chain.proceed(builder.build())
     }
 }

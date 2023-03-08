4b7718fa13b0eaff1165168e227664da58411978
이나경 <66313756+nakyoung98@users.noreply.github.com>
2023-03-08 15:35:09 +0900

    App에 AuthManager 추가

--- AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/App.kt
+++ AndroidClientDevelopment/app/src/main/java/com/nakyoung/androidclientdevelopment/App.kt
@@ -1,13 +1,19 @@
 package com.nakyoung.androidclientdevelopment
 
 import android.app.Application
-import com.nakyoung.androidclientdevelopment.api.ApiService
+import com.nakyoung.androidclientdevelopment.manager.AuthManager
+import com.nakyoung.androidclientdevelopment.service.ApiService
 
+/**
+ * 싱글톤으로 생성되어야할 것들은
+ * 모두 App에 떄려박는다.
+ * **/
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        AuthManager.init(this)
         ApiService.init(this)
     }
 }
